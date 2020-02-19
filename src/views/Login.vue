a7a68f8f5d2e7419c7f87b7d56de2513d7ad8236
Daisky <nakamura.daisky@gmail.com>
2020-02-19 21:44:37 +0900

    Login用のページを作成

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col col="4">
+      <LoginForm/>
+    </v-col>
+  </v-row>
+</template>
+<script>
+  import LoginForm from '../components/LoginForm.vue'
+  export default {
+    components: {
+      LoginForm
+    }
+  }
+</script>
